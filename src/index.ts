f5d27c15b92e95fc480a365e666c559e8f8c64c9
hoapng <giahoa01giathieu@gmail.com>
2023-09-29 18:24:21 +0700

    mongodb driver

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,5 @@
 import express from 'express'
+import databaseService from './services/database.service'
 
 const app = express()
 const port = 3000
@@ -6,6 +7,8 @@ const port = 3000
 app.get('/', (req, res) => {
   res.send('hello world')
 })
+
+databaseService.connect()
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
