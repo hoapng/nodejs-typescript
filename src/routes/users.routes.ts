258baaab6ccf04b8c71a9df536bd6338073475b1
hoapng <giahoa01giathieu@gmail.com>
2023-10-01 20:12:47 +0700

    comment

--- src/routes/users.routes.ts
+++ src/routes/users.routes.ts
@@ -6,6 +6,12 @@ import { wrapRequestHandler } from '~/utils/handlers'
 const usersRouter = Router()
 
 usersRouter.post('/login', loginValidator, wrapRequestHandler(loginController))
+/**
+ * Description. Register a new user
+ * Path: /register
+ * Method: POST
+ * Body: { name: string, email: string, password: string, confirm_password: string, date_of_birth: ISO8601 }
+ */
 usersRouter.post('/register', registerValidator, wrapRequestHandler(registerController))
 
 export default usersRouter
