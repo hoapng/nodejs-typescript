4d65a3027647842a0d5616da70f3cd6622242c42
hoapng <giahoa01giathieu@gmail.com>
2024-01-12 16:31:38 +0700

    Filter upload ảnh

--- src/controllers/medias.controllers.ts
+++ src/controllers/medias.controllers.ts
@@ -1,21 +1,10 @@
 import { NextFunction, Request, Response } from 'express'
-import formidable from 'formidable'
 import path from 'path'
+import { handleUploadSingleImage } from '~/utils/file'
 
 export const uploadSingleImageController = async (req: Request, res: Response, next: NextFunction) => {
-  // const fomidable = (await import('formidable')).default
-  const form = formidable({
-    uploadDir: path.resolve('uploads'),
-    maxFiles: 1,
-    keepExtensions: true,
-    maxFileSize: 300 * 1024 // 300KB
-  })
-  form.parse(req, (err, fields, files) => {
-    if (err) {
-      throw err
-    }
-    res.json({
-      message: 'Upload image successfully'
-    })
+  const data = await handleUploadSingleImage(req)
+  return res.json({
+    result: data
   })
 }
--- src/utils/file.ts
+++ src/utils/file.ts
@@ -1,3 +1,6 @@
+/* eslint-disable no-extra-boolean-cast */
+import { Request } from 'express'
+import formidable from 'formidable'
 import fs from 'fs'
 import path from 'path'
 
@@ -8,4 +11,32 @@ export const initFolder = () => {
       recursive: true // tạo folder nested
     })
   }
+}
+
+export const handleUploadSingleImage = (req: Request) => {
+  // const formidable = (await import('formidable')).default
+  const form = formidable({
+    uploadDir: path.resolve('uploads'),
+    maxFiles: 1,
+    keepExtensions: true,
+    maxFileSize: 300 * 1024, // 300KB
+    filter: function ({ name, originalFilename, mimetype }) {
+      const valid = name === 'image' && Boolean(mimetype?.includes('image/'))
+      if (!valid) {
+        form.emit('error' as any, new Error('File type is not valid') as any)
+      }
+      return valid
+    }
+  })
+  return new Promise((resolve, reject) => {
+    form.parse(req, (err, fields, files) => {
+      if (err) {
+        return reject(err)
+      }
+      if (!Boolean(files.image)) {
+        return reject(new Error('File is empty'))
+      }
+      resolve(files)
+    })
+  })
 }
