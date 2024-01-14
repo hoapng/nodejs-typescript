7186b10bc38ec1ac71cd5ca94e1952dfeb070951
hoapng <giahoa01giathieu@gmail.com>
2024-01-14 09:43:13 +0700

    Upload multiple images

--- src/services/medias.services.ts
+++ src/services/medias.services.ts
@@ -1,24 +1,34 @@
 import { Request } from 'express'
 import { UPLOAD_DIR } from '~/constants/dir'
-import { getNameFromFullname, handleUploadSingleImage } from '~/utils/file'
+import { getNameFromFullname, handleUploadImage } from '~/utils/file'
 import sharp from 'sharp'
 import fs from 'fs'
 import path from 'path'
 import { config } from 'dotenv'
 import { isProduction } from '~/constants/config'
+import { MediaType } from '~/constants/enums'
+import { Media } from '~/models/Other'
 
 config()
 
 class MediasService {
-  async handleUploadSingleImage(req: Request) {
-    const file = await handleUploadSingleImage(req)
-    const newName = getNameFromFullname(file.newFilename)
-    const newPath = path.resolve(UPLOAD_DIR, `${newName}.jpg`)
-    await sharp(file.filepath).jpeg().toFile(newPath)
-    fs.unlinkSync(file.filepath)
-    return isProduction
-      ? `${process.env.HOST}/static/image/${newName}.jpg`
-      : `http://localhost:${process.env.PORT}/static/image/${newName}.jpg`
+  async uploadImage(req: Request) {
+    const files = await handleUploadImage(req)
+    const result: Media[] = await Promise.all(
+      files.map(async (file) => {
+        const newName = getNameFromFullname(file.newFilename)
+        const newPath = path.resolve(UPLOAD_DIR, `${newName}.jpg`)
+        await sharp(file.filepath).jpeg().toFile(newPath)
+        fs.unlinkSync(file.filepath)
+        return {
+          url: isProduction
+            ? `${process.env.HOST}/static/image/${newName}.jpg`
+            : `http://localhost:${process.env.PORT}/static/image/${newName}.jpg`,
+          type: MediaType.Image
+        }
+      })
+    )
+    return result
   }
 }
 
