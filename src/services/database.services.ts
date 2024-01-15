890b314fe294c872a2e84a8ae9db1dbc829ec4db
hoapng <giahoa01giathieu@gmail.com>
2024-01-15 19:01:36 +0700

    Tạo tweet

--- src/services/database.services.ts
+++ src/services/database.services.ts
@@ -2,6 +2,7 @@ import { MongoClient, Db, Collection } from 'mongodb'
 import { config } from 'dotenv'
 import User from '~/models/schemas/User.schema'
 import RefreshToken from '~/models/schemas/RefreshToken.schema'
+import Tweet from '~/models/schemas/Tweet.schema'
 config()
 
 const uri = `mongodb+srv://${process.env.DB_USERNAME}:${process.env.DB_PASSWORD}@twitter.gslxgnc.mongodb.net/?retryWrites=true&w=majority`
@@ -54,6 +55,10 @@ class DatabaseService {
 
   get refreshTokens(): Collection<RefreshToken> {
     return this.db.collection(process.env.DB_REFRESH_TOKENS_COLLECTION as string)
+  }
+
+  get tweets(): Collection<Tweet> {
+    return this.db.collection(process.env.DB_TWEETS_COLLECTION as string)
   }
 }
 
